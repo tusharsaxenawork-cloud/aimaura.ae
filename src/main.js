5cb5c6af41ded784ba56be8bc79a1f18ccea7f11
tushar <tushar.saxena@devslane.com>
2026-07-08 00:06:40 +0530

    Add design and build tagline

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ document.querySelector("#app").innerHTML = `
     <section class="message">
       <p class="kicker">Coming Soon</p>
       <h1>Aimaura</h1>
+      <p class="tagline">design and build</p>
     </section>
     <footer>designed by tushar saxena</footer>
   </main>
